e39b5fab73e6a20002c24a0754a33d9dc627ec86
Alex Marshall <alexmarshall132@gmail.com>
2018-06-29 13:49:06 -0600

    Committing initial implementation of the library.

---
+++ ServiceFabricQueuedServices.Clients/QueuedWcfCommunicationClientFactory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.ServiceModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+
+namespace ServiceFabricQueuedServices.Clients
+{
+	public class QueuedWcfCommunicationClientFactory<TServiceContract> : WcfCommunicationClientFactory<TServiceContract> where TServiceContract : class
+	{
+		private readonly ChannelFactory<TServiceContract> factory;
+
+		public QueuedWcfCommunicationClientFactory(NetMessagingBinding clientBinding, TokenProvider tokenProvider, IEnumerable<IExceptionHandler> exceptionHandlers = null, IServicePartitionResolver servicePartitionResolver = null, string traceId = null) : 
+			base(clientBinding, exceptionHandlers, servicePartitionResolver, traceId, null)
+		{
+			this.factory = new ChannelFactory<TServiceContract>(clientBinding ?? throw new ArgumentNullException(nameof(clientBinding)));
+			this.factory.Endpoint.Behaviors.Add(new TransportClientEndpointBehavior(tokenProvider ?? throw new ArgumentNullException(nameof(tokenProvider))));
+		}
+
+		/// <summary>
+		/// Creates a communication client for the given endpoint address.
+		/// </summary>
+		/// <param name="endpoint">Endpoint address where the service is listening</param>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>The communication client that was created</returns>
+		protected override async Task<WcfCommunicationClient<TServiceContract>> CreateClientAsync(string endpoint, CancellationToken cancellationToken)
+		{
+			EndpointAddress endpointAddress = new EndpointAddress(endpoint);
+			TServiceContract channel = this.factory.CreateChannel(endpointAddress);
+			IClientChannel clientChannel = (IClientChannel)channel;
+			Exception connectionTimeoutException = null;
+			try
+			{
+				Task openTask = Task.Factory.FromAsync(clientChannel.BeginOpen(this.factory.Endpoint.Binding.OpenTimeout, null, null), clientChannel.EndOpen);
+				if (await Task.WhenAny(openTask, Task.Delay(this.factory.Endpoint.Binding.OpenTimeout, cancellationToken)) == openTask)
+				{
+					if (openTask.Exception != null)
+						throw openTask.Exception;
+					openTask = (Task)null;
+				}
+				else
+				{
+					clientChannel.Abort();
+					throw new TimeoutException(string.Format((IFormatProvider)CultureInfo.CurrentCulture, "Failed to open communication client after period '{0}'", new object[1]
+					{
+			(object) this.factory.Endpoint.Binding.OpenTimeout
+					}));
+				}
+			}
+			catch (AggregateException ex)
+			{
+				ex.Handle((Func<Exception, bool>)(x => x is TimeoutException));
+				connectionTimeoutException = (Exception)ex;
+			}
+			catch (TimeoutException ex)
+			{
+				connectionTimeoutException = (Exception)ex;
+			}
+			if (connectionTimeoutException != null)
+				throw new EndpointNotFoundException(connectionTimeoutException.Message, connectionTimeoutException);
+			clientChannel.OperationTimeout = this.factory.Endpoint.Binding.ReceiveTimeout;
+			return this.CreateWcfCommunicationClient(channel);
+		}
+	}
+}
---
+++ ServiceFabricQueuedServices.Clients/WcfCommunicationClientExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+
+namespace ServiceFabricQueuedServices.Clients
+{
+	/// <summary>
+	/// Utility methods for <see cref="WcfCommunicationClient{TServiceContract}"/> instances to be used
+	/// to connect to stateless services.
+	/// </summary>
+	public static class WcfCommunicationClientExtensions
+	{
+		/// <summary>
+		/// Creates a new <see cref="WcfCommunicationClient{TServiceContract}"/> capable of connection
+		/// to a stateless service with a singleton partition and default replica selection.
+		/// </summary>
+		/// <param name="wcfClientFactory">
+		/// The <see cref="WcfCommunicationClientFactory{TServiceContract}"/> to be used to create the
+		/// client. Must not be null.
+		/// </param>
+		/// <param name="fabricServiceUri">
+		/// The name of the Service Fabric service to which we're connecting. This is the URI of the service,
+		/// e.g. <code>fabric:/MyApplication/MyQueuedService</code>
+		/// </param>
+		/// <param name="retrySettings">
+		/// The settings used for retry operations. May be null, in which case default settings will be used.
+		/// </param>
+		/// <param name="cancellationToken">
+		/// The <see cref="CancellationToken"/> used to abort the call if necessary.
+		/// </param>
+		/// <typeparam name="TServiceContract">
+		/// The type of WCF Service Contract in use.
+		/// </typeparam>
+		/// <returns>
+		/// A <see cref="Task{TResult}"/> yielding a <see cref="WcfCommunicationClient{TServiceContract}"/>
+		/// ready for use with a default, queued, stateless service with a single partition. Guaranteed
+		/// not to be null.
+		/// </returns>
+		/// <exception cref="ArgumentNullException">
+		/// Thrown if <paramref name="wcfClientFactory"/> is null
+		/// -or-
+		/// Thrown if <paramref name="fabricServiceUri"/> is null.
+		/// </exception>
+		public static Task<WcfCommunicationClient<TServiceContract>> CreateDefaultClientAsync<TServiceContract>(
+			this WcfCommunicationClientFactory<TServiceContract> wcfClientFactory,
+			Uri fabricServiceUri,
+			OperationRetrySettings retrySettings = null,
+			CancellationToken? cancellationToken = null) where TServiceContract : class
+		{
+			if (wcfClientFactory == null)
+			{
+				throw new ArgumentNullException(nameof(wcfClientFactory));
+			}
+
+			if (fabricServiceUri == null)
+			{
+				throw new ArgumentNullException(nameof(fabricServiceUri));
+			}
+
+			return wcfClientFactory.GetClientAsync(
+				serviceUri: fabricServiceUri,
+				partitionKey: ServicePartitionKey.Singleton,
+				targetReplicaSelector: TargetReplicaSelector.Default,
+				listenerName: String.Empty,
+				retrySettings: retrySettings ?? new OperationRetrySettings(),
+				cancellationToken: cancellationToken ?? CancellationToken.None
+			);
+		}
+	}
+}
